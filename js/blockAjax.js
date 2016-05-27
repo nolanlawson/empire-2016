d303a2d140d180fd1da0108bdea4536f90b8c227
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-27 07:29:00 -0400

    more tweaks

--- js/blockAjax.js
+++ js/blockAjax.js
@@ -1,6 +1,6 @@
 function doAjax() {
   $.ajax({
-    url: 'http://localhost:1337/localhost:9022/package.json?nonce=' + Math.random(),
+    url: 'http://localhost:1337/localhost:9055/tmp.json?nonce=' + Math.random(),
     dataType: 'json',
     success: function (json) {
     }
